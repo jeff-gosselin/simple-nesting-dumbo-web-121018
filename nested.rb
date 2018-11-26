73ca61671d1098e4d311d764b7cce4ac1a2e7556
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-26 02:10:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,8 @@ def adding_matz
         }
      }
   
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["C"]}
+  programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
   # binding.pry
 end
 
